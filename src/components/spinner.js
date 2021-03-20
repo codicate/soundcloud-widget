7e7cdff0493c76eb654a279823632e49cd9d15d3
codicate <codicate@gmail.com>
2021-03-20 13:47:05 -0400

    spinner loading animation

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Spinner() {
-  const spinnerStyle = {
-    margin: '100px auto',
-    width: '50px',
-    height: '40px',
-    textAlign: 'center',
-    fontSize: '10px',
-  };
+const SpinnerDiv = styled.div`
+  display: flex;
+  justify-content: center;
 
-  const rectStyle = {
-    backgroundColor: '#333',
-    height: '100%',
-    width: '6px',
-    display: 'inline-block',
+  @keyframes sk-stretchdelay {
+    0%, 40%, 100% { 
+      transform: scaleY(0.4);
+    }  20% { 
+      transform: scaleY(1.0);
+    }
+  }
+`;
 
-    animation: 'sk-stretchdelay 1.2s infinite ease-in-out'
-  };
+const Rect = styled.div`
+  background-color: ${props => props.color || 'white'};
+  height: ${props => props.height || 50}px;
+  width: ${props => props.width || 6}px;
+  margin: 0 ${props => props.width / 3 || 2}px;
+
+  animation: sk-stretchdelay ${props => 0.7 + props.num / 10}s infinite ease-in-out;
+  animation-delay: -${props => 1.1 - props.index / 10}s;
+`;
+
+export default function Spinner(props) {
+  const numOfRect = props.num || 5;
 
   return (
-    <div class="spinner" style={spinnerStyle}>
-      <div class="rect" style={rectStyle}></div>
-      <div class="rect" style={rectStyle}></div>
-      <div class="rect" style={rectStyle}></div>
-      <div class="rect" style={rectStyle}></div>
-      <div class="rect" style={rectStyle}></div>
-    </div>
+    <SpinnerDiv>
+      {[...Array(numOfRect)].map((_, index) => (
+        <Rect
+          key={index}
+          index={index}
+          num={numOfRect}
+          color={props.color}
+          height={props.height}
+          width={props.width}
+        />
+      ))}
+    </SpinnerDiv>
   );
 }
